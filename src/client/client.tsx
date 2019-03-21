c50dcaafad3664e97ed86bceaee087e17aade7bc
Mario Rogic <mr@concordium.com>
2019-03-21 12:59:14 +0100

    Add dumb reload so we don't need to manually refresh

--- src/client/client.tsx
+++ src/client/client.tsx
@@ -15,3 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // app.ports.hello.subscribe(message => socket.emit('toServer', message))
 })
+
+
+// Reload every 15 minutes or so - dumb way to keep our office screens on latest UI version
+setTimeout(() => { window.location.reload() }, 15 * 60 * 1000);
