8744e05f0b6e7747546d8d7dc632874248ea2893
Mario Rogic <mr@concordium.com>
2020-03-24 16:36:53 +0000

    In prod URLs should be relative

--- src/client/elm/Config.elm
+++ src/client/elm/Config.elm
@@ -3,9 +3,11 @@ module Config exposing (..)
 
 summariesUrl =
     -- "http://127.0.0.1:12000/nodesSummary"
-    "https://dashboard.eu.staging.concordium.com/nodesSummary"
+    -- "https://dashboard.eu.staging.concordium.com/nodesSummary"
+    "/nodesSummary"
 
 
 middleware =
     -- "http://localhost:8081"
-    "https://dashboard.eu.staging.concordium.com/"
+    -- "https://dashboard.eu.staging.concordium.com/"
+    "/"
