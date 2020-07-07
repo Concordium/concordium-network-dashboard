86b696efcbf8c59095e1f86961afedc28e572a73
Michael Bisgaard Olesen <mo@concordium.com>
2020-07-07 11:27:12 +0000

    Improve node view

--- src/client/elm/NodeHelpers.elm
+++ src/client/elm/NodeHelpers.elm
@@ -6,15 +6,5 @@ import Types exposing (..)
 
 
 findNodeById : String -> Dict Host NetworkNode -> Maybe NetworkNode
-findNodeById nodeId nodes =
-    case Dict.find (\_ n -> n.nodeId == nodeId) nodes of
-        Just ( _, node ) ->
-            Just node
-
-        Nothing ->
-            Nothing
-
-
-nodeTypeById : String -> Dict Host NetworkNode -> String
-nodeTypeById nodeId nodes =
-    nodes |> findNodeById nodeId |> Maybe.map .peerType |> Maybe.withDefault "Node"
+findNodeById nodeId =
+     Dict.find (\_ n -> n.nodeId == nodeId) >> Maybe.map Tuple.second
