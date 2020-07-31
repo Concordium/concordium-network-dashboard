00aa89ca484033be5e2e2c38f4a7da708e76e831
Michael Bisgaard Olesen <mo@concordium.com>
2020-07-31 10:00:49 +0000

    Make default mode production
    Pass NODE_ENV to template to be able to see what was set server side. This is supposed to be passed to the elm app once I figure out how to do that...

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,7 +1,7 @@
 import * as express from 'express'
 import * as http from 'http'
-import { staticsRouter } from './routes/statics-router'
-import { staticsDevRouter } from './routes/statics-dev-router'
+import staticsRouter from './routes/statics-router'
+import staticsDevRouter from './routes/statics-dev-router'
 import * as config from './config'
 import { getVersion } from '../shared/utils'
 
@@ -9,12 +9,12 @@ console.log(`The App version is ${getVersion()}`)
 
 const app = express()
 
-app.use(config.IS_PRODUCTION ? staticsRouter() : staticsDevRouter())
+app.use(config.isProduction ? staticsRouter() : staticsDevRouter())
 
 const server = http.createServer(app)
 
-server.listen(config.SERVER_PORT, () => {
-  console.log(`App listening on port ${config.SERVER_PORT}!`)
+server.listen(config.serverPort, () => {
+  console.log(`App listening on port ${config.serverPort}!`)
   console.warn(`WARNING: This is the development-only backend.`)
   console.warn(`See here for the real ones:`)
   console.warn(`https://gitlab.com/Concordium/p2p-client/blob/develop/src/bin/collector.rs`)
