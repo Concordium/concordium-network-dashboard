e65031c3895ecb08e7514170484ea2cb7423c248
Emil Holm Gjørup <eg@concordium.com>
2021-02-17 09:47:01 +0100

    Add lookup page with text input

--- src/client/elm/Main.elm
+++ src/client/elm/Main.elm
@@ -19,6 +19,7 @@ import Html exposing (Html)
 import Html.Attributes exposing (style)
 import Json.Decode as D
 import Json.Encode as E
+import Lookup
 import Material.Icons.Sharp as Icon
 import Material.Icons.Types exposing (Coloring(..))
 import Network exposing (viewSummaryWidgets)
@@ -68,6 +69,7 @@ type alias Model =
     , chainModel : Chain.Model
     , explorerModel : Explorer.Model
     , toastModel : ToastModel
+    , lookupModel : Lookup.Model
     }
 
 
@@ -85,6 +87,7 @@ type Msg
     | ChainMsg Chain.Msg
     | ToggleDarkMode
     | ExplorerMsg Explorer.Msg
+    | LookupMsg Lookup.Msg
 
 
 main : Program Flags Model Msg
@@ -125,6 +128,7 @@ init flags url key =
                 , chainModel = chainInit
                 , explorerModel = Explorer.init { middlewareUrl = cfg.middlewareUrl }
                 , toastModel = { visible = False, contents = "", showCount = 0 }
+                , lookupModel = Lookup.init
                 }
     in
     ( initModel
@@ -312,6 +316,13 @@ update msg model =
             in
             ( { model | explorerModel = newExplorerModel, chainModel = chainModel }, Cmd.map ExplorerMsg newExplorerCmd )
 
+        LookupMsg lookupMsg ->
+            let
+                ( newLookupModel, cmd ) =
+                    Lookup.update lookupMsg model.lookupModel
+            in
+            ( { model | lookupModel = newLookupModel }, Cmd.map LookupMsg cmd )
+
 
 rebuildChain : ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
 rebuildChain ( model, cmd ) =
@@ -375,14 +386,17 @@ view model =
                     Network ->
                         Element.map NetworkMsg <| Network.NodesTable.view model model.networkModel
 
-                    NodeView nodeName ->
+                    NodeView _ ->
                         Element.map NetworkMsg <| Network.Node.view model model.networkModel
 
                     ChainInit ->
                         viewChain model
 
-                    ChainSelected hash ->
+                    ChainSelected _ ->
                         viewChain model
+
+                    Lookup ->
+                        viewLookup model
                 ]
         ]
     }
@@ -406,6 +420,7 @@ viewHeader ctx =
         , row [ alignRight, spacing 20, Font.color ctx.palette.fg2 ]
             [ link linkstyle { url = Route.toString Network, label = text "Network" }
             , link linkstyle { url = Route.toString ChainInit, label = text "Chain" }
+            , link linkstyle { url = Route.toString Lookup, label = text "Lookup" }
             , viewColorModeToggle ctx
             ]
         ]
@@ -454,6 +469,16 @@ viewChain model =
             ]
 
 
+viewLookup : Model -> Element Msg
+viewLookup model =
+    Widgets.content <|
+        column [ width fill, height fill, spacing 20 ]
+            [ viewSummaryWidgets model model.networkModel.nodes
+            , row [ viewCopiedToast model, centerX ] []
+            , Element.map LookupMsg <| Lookup.view { palette = model.palette, colorMode = model.colorMode } model.lookupModel
+            ]
+
+
 viewCopiedToast : Model -> Attribute Msg
 viewCopiedToast model =
     let
@@ -495,7 +520,15 @@ translateMsg msg =
 
 theme : Palette Color -> Element msg -> Html.Html msg
 theme palette elements =
-    layout
+    layoutWith
+        { options =
+            [ focusStyle
+                { borderColor = Just palette.fg1
+                , backgroundColor = Just palette.bg2
+                , shadow = Nothing
+                }
+            ]
+        }
         [ width fill
         , height fill
         , Background.color <| palette.bg1
